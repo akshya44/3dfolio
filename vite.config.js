ec93bc1786c4882aac9ea861f945b00ac04cf767
Akshya Kumar sahoo <sakshyakumar44@gmail.com>
2026-02-01 02:37:01 +0530

    Remove unused git submodules

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3dfolio/", // Assuming repo name is '3dfolio' based on package name, user can change if needed
 })
